bf11f6fa7da8bfa1f81df51efd67b141fa0de628
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-09-19 02:43:53 +0900

    fix:

--- Core Modules/lesson.js
+++ Core Modules/lesson.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 //SYNC FILE READER
 const file=fs.readFileSync('./input.txt',"utf8");
 console.log('File',file);
-console.log('************************');
+console.log('*************************');
 
 
 // SYNC FILE WRITER
 fs.writeFileSync('./input.txt',`${file} \n\t\t BekzodAli`);
 const new_file=fs.readFileSync('./input.txt',"utf8");
 console.log('File',new_file);
-console.log('************************');
+console.log('*************************');
