bdcc9f8b9f4752b172ca5c9e5937bfda3cd1577a
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-09-21 18:58:53 +0900

    feat:

--- train.js
+++ train.js
@@ -1,17 +1,39 @@
-// C-Task: 
+//D-Task
 
-// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin.
+//Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin,
+//hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method
+//ishga tushgan vaqt ham log qilinsin.
 
-// MASALAN checkSimilarity("laep", "apple") true return qiladi.
+//MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon
+//va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da
+//1ta non, 5ta lagmon va 6ta cola mavjud!
+const moment = require("moment");
 
-function checkSimilarity(text,word){
-    let index=0;
-    while(index<word.length){
-        if(!word.includes(text[index]))return false
-        index++;
+class Shop{
+    constructor(non, lagmon, cola){
+        this.non = non;
+        this.lagmon = lagmon;
+        this.cola = cola;
     }
-    return true;
+
+    qoldiq(){
+        console.log(`Hozir ${moment().format("HH:mm")}da ${this.non}ta non, ${this.lagmon}ta lag\'mon va ${this.cola}ta cola mavjud!`);
+    }
+
+    sotish(product,amount){
+        this[product]-=amount;
+        console.log(`${product} ${amount}ta ayrildi`);
+    }
+
+    qabul(product,amount){
+        this[product]+=amount;
+        console.log(`${product} ${amount}ta qo\'shildi`);
+    }
 }
 
-const result=checkSimilarity("alepp", "apple");
-console.log("Can the text be formed to word:", result);
+const shop =new Shop(4,5,2);
+
+shop.sotish("non",3);
+shop.qoldiq();
+shop.qabul('cola',2);
+shop.qoldiq()
